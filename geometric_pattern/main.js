8cd6784756655d30c03f1e9ca31518ddb1d42313
Treavon <treavonclark@gmail.com>
2017-01-24 22:21:29 +0000

    Added crazy rave lights

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,20 +1,26 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
     createCanvas(480,600);
+    
+    frameRate(5);
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius = circleDiameter/2;
+    
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 }
 
 function draw() {
-    rVal = 255;
-    gVal = 0;
-    bVal= 0;
     
     var isShifted = false;
     
@@ -24,27 +30,23 @@ function draw() {
         var x;
         
         if (isShifted) {
-             x = circleRadius
+             x = circleRadius;
         } else {
             x = 0;
         }
+        
         while (x <= width) {
-            fill(color(rVal,gVal,bVal))
-            stroke(color(rVal,gVal,bVal))
+            stroke(color(rVal,gVal,bVal));
+            fill(color(rVal,gVal,bVal));
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
+        
         y = y - circleRadius;
         isShifted = !isShifted;
         
-        rVal = rVal - 2;
-        gVal = gVal + 7;
-        bVal = bVal + 3;
+        rVal = (rVal + 254)% 256;
+        gVal = (gVal + 7)% 256;
+        bVal = (bVal + 3)% 256;
     }
-}
- function keyPressed() {
-    if (keycode === 115 || keycode === 83) {
-        saveCanvas('geometricPattern', 'png');
-    }
-    return false;
 }
