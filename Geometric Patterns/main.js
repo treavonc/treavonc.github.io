f5fb4da35e930792f32fb82a08726f57fe9e0551
Treavon <treavonclark@gmail.com>
2017-01-24 21:14:16 +0000

    Add color to pattern

--- Geometric Patterns/main.js
+++ Geometric Patterns/main.js
@@ -1,6 +1,9 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480,600);
@@ -9,6 +12,10 @@ function setup() {
 }
 
 function draw() {
+    rVal = 255;
+    gVal = 0;
+    bVal= 0;
+    
     var isShifted = false;
     
     var y = height;
@@ -21,13 +28,17 @@ function draw() {
         } else {
             x = 0;
         }
-        
         while (x <= width) {
+            fill(color(rVal,gVal,bVal))
+            stroke(color(rVal,gVal,bVal))
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
-        
         y = y - circleRadius;
         isShifted = !isShifted;
+        
+        rVal = rVal - 2;
+        gVal = gVal + 7;
+        bVal = bVal + 3;
     }
 }
